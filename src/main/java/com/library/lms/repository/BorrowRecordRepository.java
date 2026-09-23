23add535153a7f45ca92388eec1177797141adfe
Harshit-0018 <harshitsingh8157@gmail.com>
2026-09-23 19:56:24 +0530

    Add repositories

---
+++ src/main/java/com/library/lms/repository/BorrowRecordRepository.java
@@ -0,0 +1,9 @@
+package com.library.lms.repository;
+
+import com.library.lms.entity.BorrowRecord;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BorrowRecordRepository extends JpaRepository<BorrowRecord, Long> {
+}
